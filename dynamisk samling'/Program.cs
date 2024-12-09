8b0a7e97589081adcd2b7918b63e6932c770d126
Lias <lias.israelsson@edu.lycksele.se>
2024-12-09 13:33:04 +0100

    first commit

---
+++ dynamisk samling'/Program.cs
@@ -0,0 +1,61 @@
+﻿using System.Runtime.InteropServices;
+
+public class Samling<T>{
+    int buffert;
+    T[] Listan;
+    int längd;
+    int antal;
+
+public void expandera(int storlek){
+if(storlek<1){
+return;
+}else{
+    längd = längd + storlek;
+    T[] temp = new T[längd];
+for(int i = 0; i < antal; i++){
+temp[i] = Listan[i];
+}
+Listan = temp;
+}
+}
+
+public void reducera(){
+    längd = antal;
+    T[] temp = new T[längd];
+for(int i = 0; i < antal; i++){
+temp[i] = Listan[i];
+}
+Listan = temp;
+}
+
+public void läggtill(T b){
+if(antal == längd){
+expandera(1);
+}
+Listan[antal] = b;
+}
+
+public void print(){
+    for(int i = 0; i < antal; i++){
+Console.WriteLine(Listan[i]);
+}
+}
+    public Samling(){
+buffert = 30;
+Listan = new T[längd];
+längd = 30;
+antal = 0;
+    }
+
+}
+
+class Program{
+    static void Main(){
+Samling<bool> MinSamling= new Samling<bool>();
+MinSamling.läggtill(true);
+MinSamling.läggtill(false);
+MinSamling.reducera();
+MinSamling.print();
+    }
+}
+
